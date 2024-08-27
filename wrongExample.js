0e7182bf3c5dc331455140064bb0c255e88cc91c
Nikolaos Prevenios <145902129+nikospr90@users.noreply.github.com>
2024-08-27 09:47:42 +0200

    Update wrongExample.js

--- wrongExample.js
+++ wrongExample.js
@@ -1,6 +1,6 @@
 let x = 5
 let y = 7
 function double(x) {return x^2}
-function double(x) {return y^2}
+function double(y) {return y^2}
 Console.Print('Dubbel av 5 är: ' + double(x));
 Console.Print('Dubbel av 5 är: ' + double(y));
